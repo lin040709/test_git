3937a9797c4c996115202bd3e2c75eac464a26a3
zf lin <2870557699@qq.com>
2024-09-10 21:53:21 +0800

    message

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ class git{
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("'abc");
-        System.out.println("123");
+        
     }
 }
