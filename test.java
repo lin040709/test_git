ff0976d8b74d5431f4c3f7099dbfc3f3159cd7ce
zf lin <2870557699@qq.com>
2024-09-10 15:26:59 +0800

    66

--- test.java
+++ test.java
@@ -1,3 +1,6 @@
-public static void main(String[] args) {
-    System.out.println("1");
+class git{
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("'abc");
+    }
 }
